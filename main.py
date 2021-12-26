3c1f897453e24bf45d2b78fe48097b2af4f9625a
Nutifafa Afi Attor <attorfafa@gmail.com>
2021-12-26 12:48:12 +0000

    Update

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 # Mad Libs
 
+"""
+String Concatenation in Python
+Assuming there is a variable called name, one can concatenate the value of name to a string in three ways:
+name = "Nutifafa"
+
+1. print("my name is " + name)
+2. print("my name is {}".format(name))
+3. print(f"my name is {name}")
+
+"""
+
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famousPerson = input("A famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me excited all the time because I love to {verb1}. " \
+         f"Stay hydrated and {verb2} like you are {famousPerson}!"
+
+print(madlib)
